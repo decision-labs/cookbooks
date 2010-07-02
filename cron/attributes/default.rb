95a7ce18d002e4fff3186ac52eda5cea5a09748d
Benedikt Böhm <bb@xnull.de>
2010-07-02 23:55:13 +0200

    set_unless and default are not the same as of chef 0.9

--- cron/attributes/default.rb
+++ cron/attributes/default.rb
@@ -1,14 +1,14 @@
 default[:contacts][:cron] = "root@#{node[:fqdn]}"
 
-default[:cron][:hourly][:minute] = rand(60)
+set_unless[:cron][:hourly][:minute] = rand(60)
 
-default[:cron][:daily][:minute] = rand(30)
-default[:cron][:daily][:hour] = 3
+set_unless[:cron][:daily][:minute] = rand(30)
+set_unless[:cron][:daily][:hour] = 3
 
-default[:cron][:weekly][:minute] = 15 + rand(30)
-default[:cron][:weekly][:hour] = 4
-default[:cron][:weekly][:wday] = 6
+set_unless[:cron][:weekly][:minute] = 15 + rand(30)
+set_unless[:cron][:weekly][:hour] = 4
+set_unless[:cron][:weekly][:wday] = 6
 
-default[:cron][:monthly][:minute] = 30 + rand(30)
-default[:cron][:monthly][:hour] = 5
-default[:cron][:monthly][:day] = 1
+set_unless[:cron][:monthly][:minute] = 30 + rand(30)
+set_unless[:cron][:monthly][:hour] = 5
+set_unless[:cron][:monthly][:day] = 1
