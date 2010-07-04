26a81bc4b8cbf0298d6a0077f4ce5657109b590c
Benedikt Böhm <bb@xnull.de>
2010-07-04 09:53:17 +0200

    fix account definition

--- account/definitions/account.rb
+++ account/definitions/account.rb
@@ -1,29 +1,61 @@
-define :account, :uid => nil, :gid => nil, :shell => nil, :comment => nil, :home_mode => "0750", :authorized_keys => [] do
-  home = "/home/#{params[:name]}"
+define :account,
+       :uid => nil,
+       :gid => "users",
+       :groups => [],
+       :shell => nil,
+       :comment => nil,
+       :password => "!",
+       :home => nil,
+       :home_mode => "0750",
+       :authorized_keys => [],
+       :action => :create do
+  include_recipe "account"
+
+  home = params[:home]
+  home = "/home/#{params[:name]}" if not home
 
   user params[:name] do
-    shell params['shell']
-    comment params['comment']
+    uid params[:uid]
+    gid params[:gid]
+    shell params[:shell]
+    comment params[:comment]
+    password params[:password]
     home home
+    action params[:action]
+  end
+
+  if params[:action] == :create
+    params[:groups].each do |g|
+      group g do
+        members params[:name]
+        append true
+      end
+    end
+
+    home_owner = params[:name]
+    home_group = "users"
+  else
+    home_owner = "root"
+    home_group = "root"
   end
 
   directory home do
-    owner params[:name]
-    group "users"
+    owner home_owner
+    group home_group
     mode params[:home_mode]
   end
 
   directory "#{home}/.ssh" do
-    owner params[:name]
-    group "users"
+    owner home_owner
+    group home_group
     mode "0700"
   end
 
   template "#{home}/.ssh/authorized_keys" do
     source "authorized_keys.erb"
     cookbook "account"
-    owner params[:name]
-    group "users"
+    owner home_owner
+    group home_group
     mode "0600"
     variables(:authorized_keys => params[:authorized_keys])
   end
--- account/recipes/hostmasters.rb
+++ account/recipes/hostmasters.rb
@@ -4,8 +4,14 @@ hostmasters.each do |login|
   hostmaster = data_bag_item('hostmasters', login)
 
   account login do
+    password hostmaster['password']
     shell hostmaster['shell']
     comment hostmaster['comment']
     authorized_keys hostmaster['authorized_keys']
+  end
+
+  group "wheel" do
+    members login
+    append true
   end
 end
