076048d5183ee73fb689132c578022af72672b7f
Benedikt Böhm <bb@xnull.de>
2010-07-05 12:46:50 +0200

    add gitosis cookbook

---
+++ gitosis/files/default/git-init-bare-empty.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+GIT_REPO=$1
+OWNER=${2:-$(id -u)}
+GROUP=${3:-$(id -g)}
+TMPDIR=$(mktemp -d)
+
+mkdir -p $GIT_REPO
+pushd $GIT_REPO
+git init --bare
+popd
+
+pushd $TMPDIR
+git init
+touch .keep
+git add .keep
+git commit -m "initial commit"
+git remote add origin file://$GIT_REPO
+git push origin master
+popd
+
+rm -rf $TMPDIR
+
+chown $OWNER:$GROUP -R $GIT_REPO
