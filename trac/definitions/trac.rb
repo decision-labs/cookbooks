da9314772628dbb9d4518f6d0e88ab00faf60fa8
Gerrit Riessen <gerrit.riessen@gmail.com>
2010-07-07 13:08:59 +0200

    minro changes to the trac defaults

--- trac/definitions/trac.rb
+++ trac/definitions/trac.rb
@@ -1,4 +1,5 @@
 define :trac, :action => :create do
+  ## require recipe mysql
   include_recipe "trac"
 
   mysql_database "trac_#{params[:name]}" do
@@ -9,16 +10,21 @@ define :trac, :action => :create do
   basedir = "/var/lib/trac"
 
   trac_name = params[:name]
-  trac_dir = "#{basedir}/#{trac_name}"
+  trac_dir  = "#{basedir}/#{trac_name}"
   trac_pass = get_password("mysql/trac")
-  trac_dsn = "mysql://trac:#{trac_pass}@localhost:3306/trac_#{trac_name}"
-  git_dir = "/var/spool/gitosis/repositories/#{trac_name}.git"
+  trac_dsn  = "mysql://trac:#{trac_pass}@localhost:3306/trac_#{trac_name}"
+  git_dir   = "/var/spool/gitosis/repositories/#{trac_name}.git"
 
   if params[:action] == :create
     gitosis_repo trac_name
 
     execute "tracinit-#{trac_name}" do
-      command "trac-admin #{trac_dir} initenv --inherit=/etc/trac/trac.ini #{trac_name} #{trac_dsn} git #{git_dir} && trac-admin #{trac_dir} permission add admin TRAC_ADMIN && chown tracd:tracd -R #{trac_dir} && mysql trac_#{trac_name} < /etc/trac/initial.sql"
+      command [("trac-admin #{trac_dir} initenv --inherit=/etc/trac/trac.ini "+
+                "#{trac_name} #{trac_dsn} git #{git_dir}"),
+               "trac-admin #{trac_dir} permission add admin TRAC_ADMIN",
+               "chown tracd:tracd -R #{trac_dir}",
+               "mysql trac_#{trac_name} < /etc/trac/initial.sql"
+              ].join(" && ")
       creates trac_dir
     end
   else
