822edce77d628686cb3292012dce6d53e80df630
Benedikt Böhm <bb@xnull.de>
2010-07-22 09:31:53 +0200

    use mongo 1.4.4

--- mongodb/recipes/default.rb
+++ mongodb/recipes/default.rb
@@ -1,14 +1,7 @@
-#
-# Cookbook Name:: mongo
-# Recipe:: default
-#
-# Copyright 2010, Submarine Internet GmbH
-#
-
 include_recipe "portage"
 
 portage_package_keywords "=dev-lang/spidermonkey-1.7.0-r1"
-portage_package_keywords "=dev-db/mongodb-1.4.3"
+portage_package_keywords "=dev-db/mongodb-1.4.4"
 
 package "dev-db/mongodb"
 
