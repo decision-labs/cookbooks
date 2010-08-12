9dc1039c13dbcd0e8504b49840a851c9f5dc050c
Gerrit Riessen <gerrit.riessen@gmail.com>
2010-08-12 12:02:22 +0200

    Add nginx definition for creating domain redirects.
    E.g.
    
      nagios_redirect("redirect example.org") do
        from_to("example.org" => "example.com")
        opts( :type => :permanent, :ports => [80,443] )
      end
    
    Generates a nginx conf (in /etc/nginx/servers/) with:
    
    =====================================================
    server {
        listen 80;
        server_name .example.org;
        rewrite ^/(.*) http://example.com permanent;
    }
    
    server {
        listen 443;
        server_name .example.org;
        rewrite ^/(.*) https://example.com permanent;
    }
    =====================================================
    
    'opts' are optional and defaults are shown in the example.

---
+++ nagios/definitions/nagios_redirect.rb
@@ -0,0 +1,22 @@
+define :nagios_redirect, :from_to => {}, :opts => {} do
+  from_host, to_host = params[:from_to].to_a.first
+  opts = { :type => :permanent, :ports => [80,443] }.merge(params[:opts] || {})
+
+  file_content = opts[:ports].collect do |port|
+    ssl = (port == 443 ? "s" : "")
+    <<-EOF
+server {
+    listen #{ port };
+    server_name .#{ from_host };
+    rewrite ^/(.*) http#{ ssl }://#{ to_host } #{ opts[:type] };
+}
+    EOF
+  end.join("\n")
+  
+  file("/etc/nginx/servers/redirect_%s-%s.conf" % [from_host,to_host]) do
+    owner "root"
+    group "root"
+    mode "644"
+    content file_content
+  end
+end
