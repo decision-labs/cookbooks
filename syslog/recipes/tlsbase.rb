63248832e3886acbdeed3ed0e07a1b19aa7f464f
Benedikt Böhm <bb@xnull.de>
2010-08-21 15:03:27 +0200

    use ssl_ca helper

--- syslog/recipes/tlsbase.rb
+++ syslog/recipes/tlsbase.rb
@@ -1,28 +1,15 @@
 include_recipe "openssl"
 
-%w(
-  /etc/ssl/syslog-ng
-  /etc/ssl/syslog-ng/ca.d
-).each do |d|
-  directory d do
-    owner "root"
-    group "root"
-    mode "0750"
-    recursive true
-  end
-end
-
-cookbook_file "/etc/ssl/syslog-ng/ca.d/ca.crt" do
-  source "certificates/ca.crt"
-  cookbook "openssl"
+directory "/etc/ssl/syslog-ng/ca.d" do
   owner "root"
   group "root"
-  mode "0640"
+  mode "0750"
+  recursive true
 end
 
-execute "syslog-ca-symlink" do
-  command "ln -s ca.crt /etc/ssl/syslog-ng/ca.d/`openssl x509 -noout -hash -in /etc/ssl/syslog-ng/ca.d/ca.crt`.0"
-  not_if "test -e /etc/ssl/syslog-ng/ca.d/`openssl x509 -noout -hash -in /etc/ssl/syslog-ng/ca.d/ca.crt`.0"
+ssl_ca "/etc/ssl/syslog-ng/ca.d/ca" do
+  symlink true
+  notifies :restart, resources(:service => "syslog-ng")
 end
 
 ssl_certificate "/etc/ssl/syslog-ng/machine" do
