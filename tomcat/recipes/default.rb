9a5150a5d5957a4a4ac7ac52803ef5baef5a6858
Gerrit Riessen <gerrit.riessen@gmail.com>
2010-09-12 12:29:26 +0200

    added tomcat configuration stuff. included here are the default settings.

--- tomcat/recipes/default.rb
+++ tomcat/recipes/default.rb
@@ -1,3 +1,20 @@
 include_recipe "java"
 
 package "www-servers/tomcat"
+
+service "tomcat-6" do
+  action [ :enable, :start ]
+end
+
+{ 
+  "localhost.manager.xml" => "/etc/tomcat-6/Catalina/localhost/manager.xml",
+  "tomcat-users.xml"      => "/etc/tomcat-6/tomcat-users.xml"
+}.each do |src, dest|
+  cookbook_file dest do
+    source src
+    owner "tomcat"
+    group "tomcat"
+    mode "0644"
+    notifies :restart, resources(:service => "tomcat-6")
+  end
+end
