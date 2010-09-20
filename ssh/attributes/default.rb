8765efc460dfa446b157b233dd58b1e975ad4d56
Gerrit Riessen <gerrit.riessen@gmail.com>
2010-09-20 10:35:20 +0200

    Added max auth tries setting to sshd.

--- ssh/attributes/default.rb
+++ ssh/attributes/default.rb
@@ -4,3 +4,4 @@ default[:ssh][:server][:root_login] = "no"
 default[:ssh][:server][:x11_forwarding] = "no"
 default[:ssh][:server][:use_lpk] = "no"
 default[:ssh][:server][:allow_local_root] = "no"
+default[:ssh][:server][:max_auth_tries] = 6
