dd3009d4767cbfd4badbedab45b8862de9b776a9
Benedikt Böhm <bb@xnull.de>
2010-10-02 19:13:33 +0200

    fix example user

---
+++ databags/users/john.rb
@@ -0,0 +1,7 @@
+self[:comment] = "John Doe"
+self[:email] = "john@example.com"
+self[:groups] = [ "hostmaster" ]
+self[:shell] = "/bin/bash"
+self[:authorized_keys] = [
+  'ssh-rsa ... john@linux'
+]
