3b0f54422940c6803c1336f772f0f2b07d3157bd
Benedikt Böhm <bb@xnull.de>
2010-10-09 21:20:45 +0200

    install proper certificates for nagios

--- nagios/recipes/server.rb
+++ nagios/recipes/server.rb
@@ -92,6 +92,12 @@ end
 
 node[:apache][:default_redirect] = "https://#{node[:fqdn]}"
 
+ssl_ca "/etc/ssl/apache2/ca"
+
+ssl_certificate "/etc/ssl/apache2/server" do
+  cn node.fqdn
+end
+
 apache_vhost "nagios" do
   template "nagios.vhost.conf.erb"
 end
