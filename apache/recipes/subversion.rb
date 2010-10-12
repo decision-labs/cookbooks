cc0dce7b6207e8c124be26d60a0ed0db8f15b965
Benedikt Böhm <bb@xnull.de>
2010-10-12 09:37:23 +0200

    refactor mod_dav into a proper recipe

--- apache/recipes/subversion.rb
+++ apache/recipes/subversion.rb
@@ -1,10 +1,6 @@
 include_recipe "portage"
 include_recipe "apache"
-
-portage_package_use "www-servers/apache|dav" do
-  package "www-servers/apache"
-  use "apache2_modules_dav apache2_modules_dav_fs apache2_modules_dav_lock"
-end
+include_recipe "apache::dav"
 
 portage_package_use "dev-vcs/subversion" do
   use "apache2"
