29449e42e21eb8f0374ba50b9ee6b5cee71f58f2
Benedikt Böhm <bb@xnull.de>
2010-10-19 15:59:51 +0200

    add monit cookbook

---
+++ monit/recipes/default.rb
@@ -0,0 +1,32 @@
+package "app-admin/monit"
+
+service "monit" do
+  supports :restart => true, :status => true
+  action [ :enable, :start ]
+end
+
+execute "monit reload" do
+  command "/usr/bin/monit reload"
+  action :nothing
+end
+
+directory "/etc/monit.d" do
+  owner "root"
+  group "root"
+  mode "0700"
+end
+
+file "/etc/monit.d/dummy" do
+  content ""
+  owner "root"
+  group "root"
+  mode "0600"
+end
+
+template "/etc/monitrc" do
+  source "monitrc"
+  owner "root"
+  group "root"
+  mode "0600"
+  notifies :restart, resources(:service => "monit")
+end
