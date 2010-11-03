c16540e0f4921a939c0f3be0740257653f89db50
Benedikt Böhm <bb@xnull.de>
2010-11-03 14:45:41 +0100

    add imagemagick cookbook

---
+++ imagemagick/recipes/default.rb
@@ -0,0 +1,19 @@
+include_recipe "portage"
+
+file "/etc/portage/package.use/chef-x11-libs-cairo" do
+  action :delete
+end
+
+portage_package_unmask   "=dev-libs/glib-2.25.17"
+portage_package_keywords "=dev-libs/glib-2.25.17"
+
+portage_package_unmask   "=x11-libs/gdk-pixbuf-2.22.0"
+portage_package_keywords "=x11-libs/gdk-pixbuf-2.22.0"
+
+portage_package_unmask   "=gnome-base/librsvg-2.31.0"
+portage_package_keywords "=gnome-base/librsvg-2.31.0"
+portage_package_use      "gnome-base/librsvg" do
+  use %w(-gtk)
+end
+
+package "media-gfx/imagemagick"
