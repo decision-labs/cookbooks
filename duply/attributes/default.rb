1ccdaeb023899b42f22633619599a2505d03a617
Benedikt Böhm <bb@xnull.de>
2010-11-22 13:22:44 +0100

    duply: keep 3 months of full backups

--- duply/attributes/default.rb
+++ duply/attributes/default.rb
@@ -5,7 +5,7 @@ set_unless[:backup][:configs] = Mash.new
 
 node[:backup][:configs].each do |name, params|
   set[:backup][:configs][name][:name] = name
-  set_unless[:backup][:configs][name][:max_full_backups] = 1
+  set_unless[:backup][:configs][name][:max_full_backups] = 3
   set_unless[:backup][:configs][name][:max_full_age] = "1M"
   set_unless[:backup][:configs][name][:volume_size] = "25"
 end
