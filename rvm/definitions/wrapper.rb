cf4c17e184a29749512810a1524e694240ada886
Benedikt Böhm <bb@xnull.de>
2010-11-28 10:58:05 +0100

    rvm: make sure HOME is set in rvm_wrapper scripts

--- rvm/definitions/wrapper.rb
+++ rvm/definitions/wrapper.rb
@@ -1,12 +1,15 @@
-define :rvm_wrapper, :code => "", :owner => "root", :group => "root", :mode => "0755" do
+define :rvm_wrapper, :code => "", :user => "root", :mode => "0755" do
+  rvm = node[:rvm][:instance][params[:user]]
+
   file params[:name] do
     content <<-EOS
 #!/bin/bash
+export HOME=~#{rvm[:user]}
 source /usr/local/rvm/scripts/rvm
 #{params[:code]}
 EOS
-    owner params[:owner]
-    group params[:group]
+    owner rvm[:user]
+    group rvm[:group]
     mode params[:mode]
   end
 end
