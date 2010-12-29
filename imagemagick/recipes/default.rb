f95e57ed9da78a84349a333360f12a47204e16ce
Benedikt Böhm <bb@xnull.de>
2010-12-29 12:12:23 +0100

    imagemagick: keyword new versions

--- imagemagick/recipes/default.rb
+++ imagemagick/recipes/default.rb
@@ -4,11 +4,14 @@ file "/etc/portage/package.use/chef-x11-libs-cairo" do
   action :delete
 end
 
-portage_package_unmask   "=dev-libs/glib-2.25.17"
-portage_package_keywords "=dev-libs/glib-2.25.17"
+portage_package_unmask   "=dev-libs/glib-2.26.1"
+portage_package_keywords "=dev-libs/glib-2.26.1"
 
 portage_package_unmask   "=x11-libs/gdk-pixbuf-2.22.0"
 portage_package_keywords "=x11-libs/gdk-pixbuf-2.22.0"
+portage_package_use      "x11-libs/gdk-pixbuf" do
+  use %w(-X)
+end
 
 portage_package_unmask   "=gnome-base/librsvg-2.32.1"
 portage_package_keywords "=gnome-base/librsvg-2.32.1"
