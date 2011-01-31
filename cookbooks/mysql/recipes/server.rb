5152683e9776bf9ca9767d9d1a04012c41cc97a4
Benedikt Böhm <bb@xnull.de>
2011-01-31 21:38:14 +0100

    mysql: disable some service checks by default

--- cookbooks/mysql/recipes/server.rb
+++ cookbooks/mysql/recipes/server.rb
@@ -165,29 +165,34 @@ if tagged?("nagios-client")
     content "#!/bin/bash\nexec /usr/lib/nagios/plugins/check_mysql_health --hostname localhost --username nagios --password #{mysql_nagios_password} \"$@\""
   end
 
-  { # name          command               warn crit check note period
-    :ctime    => %w(connection-time       1    5    1     15   24x7),
-    :conns    => %w(threads-connected     75   100  1     15   24x7),
-    :tchit    => %w(threadcache-hitrate   90:  80:  60    180  never),
-    :qchit    => %w(qcache-hitrate        90:  80:  60    180  never),
-    :qclow    => %w(qcache-lowmem-prunes  1    10   60    180  never),
-    :slow     => %w(slow-queries          0.1  1    60    60   24x7),
-    :long     => %w(long-running-procs    10   20   5     60   24x7),
-    :tabhit   => %w(tablecache-hitrate    99:  95:  60    180  never),
-    :lock     => %w(table-lock-contention 1    2    60    180  24x7),
-    :index    => %w(index-usage           90:  80:  60    60   never),
-    :tmptab   => %w(tmp-disk-tables       25   50   60    180  never),
-    :kchit    => %w(keycache-hitrate      99:  95:  60    180  never),
-    :bphit    => %w(bufferpool-hitrate    99:  95:  60    180  never),
-    :bpwait   => %w(bufferpool-wait-free  1    10   60    180  never),
-    :logwait  => %w(log-waits             1    10   60    180  never),
-    :slaveio  => %w(slave-io-running      0    0    1     15   24x7),
-    :slavesql => %w(slave-sql-running     0    0    1     15   24x7),
-    :slavelag => %w(slave-lag             60   120  5     60   24x7),
+  { # name          command               warn crit check note enabled period
+    :ctime    => %w(connection-time       1    5    1     15   1       24x7),
+    :conns    => %w(threads-connected     75   100  1     15   1       24x7),
+    :tchit    => %w(threadcache-hitrate   90:  80:  60    180  1       never),
+    :qchit    => %w(qcache-hitrate        90:  80:  60    180  0       never),
+    :qclow    => %w(qcache-lowmem-prunes  1    10   60    180  0       never),
+    :slow     => %w(slow-queries          0.1  1    60    60   1       24x7),
+    :long     => %w(long-running-procs    10   20   5     60   1       24x7),
+    :tabhit   => %w(tablecache-hitrate    99:  95:  60    180  1       never),
+    :lock     => %w(table-lock-contention 1    2    60    180  1       24x7),
+    :index    => %w(index-usage           90:  80:  60    60   0       never),
+    :tmptab   => %w(tmp-disk-tables       25   50   60    180  1       never),
+    :kchit    => %w(keycache-hitrate      99:  95:  60    180  0       never),
+    :bphit    => %w(bufferpool-hitrate    99:  95:  60    180  1       never),
+    :bpwait   => %w(bufferpool-wait-free  1    10   60    180  1       never),
+    :logwait  => %w(log-waits             1    10   60    180  1       never),
+    :slaveio  => %w(slave-io-running      0    0    1     15   1       24x7),
+    :slavesql => %w(slave-sql-running     0    0    1     15   1       24x7),
+    :slavelag => %w(slave-lag             60   120  5     60   1       24x7),
   }.each do |name, p|
     name = name.to_s
     command_name = "check_mysql_#{name}"
     service_name = "MYSQL-#{name.upcase}"
+    enabled = if p[5] == "1"
+                true
+              else
+                false
+              end
 
     nrpe_command command_name do
       command "/usr/lib/nagios/plugins/check_mysql_health_wrapper --mode #{p[0]} --warning #{p[1]} --critical #{p[2]}"
@@ -197,8 +202,9 @@ if tagged?("nagios-client")
       check_command "check_nrpe!check_mysql_#{name}"
       check_interval p[3]
       notification_interval p[4]
-      notification_period p[5]
+      notification_period p[6]
       servicegroups "mysql"
+      enabled enabled
     end
 
     nagios_service_dependency service_name do
