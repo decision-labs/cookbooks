e2c560c0e2235bda3bcd57702101b70cdda41ecc
Benedikt Böhm <bb@xnull.de>
2011-02-01 14:33:24 +0100

    mongodb: add nagios service checks

--- cookbooks/mongodb/recipes/default.rb
+++ cookbooks/mongodb/recipes/default.rb
@@ -4,3 +4,12 @@ portage_package_keywords "=dev-lang/spidermonkey-1.7.0-r1"
 portage_package_keywords "=dev-db/mongodb-1.6.3"
 
 package "dev-db/mongodb"
+
+if tagged?("nagios-client")
+  portage_package_keywords "=dev-python/pymongo-1.9"
+  package "dev-python/pymongo"
+
+  nagios_plugin "mongodb" do
+    source "check_mongodb"
+  end
+end
--- cookbooks/mongodb/recipes/server.rb
+++ cookbooks/mongodb/recipes/server.rb
@@ -14,3 +14,48 @@ template "/etc/conf.d/mongodb" do
   mode "0644"
   notifies :restart, resources(:service => "mongodb")
 end
+
+if tagged?("nagios-client")
+  nrpe_command "check_mongodb" do
+    command "/usr/lib/nagios/plugins/check_pidfile /var/run/mongodb/mongodb.pid mongod"
+  end
+
+  nagios_service "MONGODB" do
+    check_command "check_nrpe!check_mongodb"
+    servicegroups "mongodb"
+  end
+
+  { # name             command         warn crit check note
+    :connect     => %w(connect         2    5    1     15),
+    :connections => %w(connections     80   90   1     15),
+    #:flush       => %w(flushing        2    5    60    180),
+    :lock        => %w(lock            2    5    60    180),
+    #:memory      => %w(memory          2    5    5     180),
+    :repl_lag    => %w(replication_lag 2    5    60    180),
+    :repl_state  => %w(replset_state   0    0    1     15),
+  }.each do |name, p|
+      name = name.to_s
+      command_name = "check_mongo_#{name}"
+      service_name = "MONGODB-#{name.upcase.gsub(/_/, '-')}"
+
+    nrpe_command command_name do
+      command "/usr/lib/nagios/plugins/check_mongodb -H localhost -P 27017 -A #{p[0]} -W #{p[1]} -C #{p[2]}"
+    end
+
+    nagios_service service_name do
+      check_command "check_nrpe!#{command_name}"
+      check_interval p[3]
+      notification_interval p[4]
+      servicegroups "mongodb"
+    end
+
+    nagios_service_dependency service_name do
+      depends %w(MONGODB)
+    end
+  end
+
+  unless node[:mongodb][:replication][:set]
+    node.default[:nagios][:services]["MONGODB-REPL-STATE"][:enabled] = false
+    node.default[:nagios][:services]["MONGODB-REPL-LAG"][:enabled] = false
+  end
+end
