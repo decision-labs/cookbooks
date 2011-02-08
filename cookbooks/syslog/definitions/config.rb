7f05ea421280d3bab0e00d690a5c479c15e5dcc7
Benedikt Böhm <bb@xnull.de>
2011-02-08 22:21:46 +0100

    rvm: add syslog config for passengers production.log

--- cookbooks/syslog/definitions/config.rb
+++ cookbooks/syslog/definitions/config.rb
@@ -12,6 +12,7 @@ define :syslog_config, :source => nil, :variables => {} do
     group "root"
     mode "0644"
     source template
+    cookbook params[:cookbook] if params[:cookbook]
     variables params[:variables]
     notifies :restart, "service[syslog-ng]"
   end
