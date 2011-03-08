6ad3e3a570a5d934dc7b488d4adc211b66dcf607
Benedikt Böhm <bb@xnull.de>
2011-03-08 10:29:14 +0100

    apache: add attribute for error_log setting

--- cookbooks/apache/attributes/default.rb
+++ cookbooks/apache/attributes/default.rb
@@ -2,3 +2,4 @@ default[:apache][:apr_util][:use] = Array.new
 default[:apache][:mpm] = "prefork"
 default[:apache][:default_redirect] = nil
 default[:apache][:ssl][:enabled] = false
+default[:apache][:error_log] = "syslog:daemon"
