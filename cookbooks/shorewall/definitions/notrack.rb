3ce1bf4d079cc0c200ef1fffe2f1e9946c8a7cdc
Benedikt Böhm <bb@xnull.de>
2011-04-21 12:06:27 +0200

    shorewall: switch to the new net-firewall/shorewall ebuild and refactor recipes for better IPv6 support

--- cookbooks/shorewall/definitions/notrack.rb
+++
@@ -1,8 +0,0 @@
-define :shorewall_notrack,
-  :source => "net",
-  :dest => "$FW",
-  :proto => "tcp",
-  :destport => "-",
-  :sourceport => "-" do
-  node[:shorewall][:notrack][params[:name]] = params
-end
--- cookbooks/shorewall/recipes/default.rb
+++ cookbooks/shorewall/recipes/default.rb
@@ -1,40 +1,16 @@
-package "net-firewall/shorewall" do
-  action :remove
-end
-
-if node[:shorewall][:perl]
-  package "net-firewall/shorewall-perl"
-else
-  package "net-firewall/shorewall-shell"
-end
-
 # reset all attributes to make sure cruft is being deleted on chef-client run
 node[:shorewall][:hosts] = {}
+node[:shorewall6][:hosts] = {}
 node[:shorewall][:interfaces] = {}
-node[:shorewall][:notrack] = {}
+node[:shorewall6][:interfaces] = {}
 node[:shorewall][:policies] = {}
+node[:shorewall6][:policies] = {}
 node[:shorewall][:rules] = {}
+node[:shorewall6][:rules] = {}
 node[:shorewall][:tunnels] = {}
+node[:shorewall6][:tunnels] = {}
 node[:shorewall][:zones] = {}
-
-execute "shorewall-restart" do
-  command "/sbin/shorewall restart"
-  action :nothing
-end
-
-directory "/etc/shorewall" do
-  owner "root"
-  group "root"
-  mode "0700"
-end
-
-template "/etc/shorewall/shorewall.conf" do
-  source "shorewall.conf"
-  owner "root"
-  group "root"
-  mode "0600"
-  notifies :run, "execute[shorewall-restart]"
-end
+node[:shorewall6][:zones] = {}
 
 include_recipe "shorewall::rules"
 
@@ -46,6 +22,14 @@ if tagged?("pkgsync-client")
       dest "$FW:#{node[:ipaddress]}"
       destport "rsync"
     end
+
+    if n[:ip6address]
+      shorewall6_rule "pkgsync-master@#{n[:fqdn]}" do
+        source "net:<#{n[:ip6address]}>"
+        dest "$FW:<#{node[:ip6address]}>"
+        destport "rsync"
+      end
+    end
   end
 end
 
@@ -55,6 +39,13 @@ search(:node, "tags:nagios-master").each do |n|
     source "net:#{n[:ipaddress]}"
     destport "4949,5666"
   end
+
+  if n[:ip6address]
+    shorewall6_rule "nagios-master@#{n[:fqdn]}" do
+      source "net:<#{n[:ip6address]}>"
+      destport "5666"
+    end
+  end
 end
 
 # munin rules
@@ -63,29 +54,19 @@ search(:node, "tags:munin-master").each do |n|
     source "net:#{n[:ipaddress]}"
     destport "4949"
   end
-end
 
-%w(
-  hosts
-  interfaces
-  notrack
-  params
-  policy
-  rules
-  tunnels
-  zones
-).each do |t|
-  template "/etc/shorewall/#{t}" do
-    source "#{t}.erb"
-    owner "root"
-    group "root"
-    mode "0600"
-    notifies :run, "execute[shorewall-restart]"
+  if n[:ip6address]
+    shorewall6_rule "munin-master@#{n[:fqdn]}" do
+      source "net:<#{n[:ip6address]}>"
+      destport "4949"
+    end
   end
 end
 
-service "shorewall" do
-  action [:enable, :start]
+include_recipe "shorewall::ipv4"
+
+if node[:ipv6_enabled]
+  include_recipe "shorewall::ipv6"
 end
 
 nagios_plugin "conntrack" do
@@ -105,12 +86,3 @@ end
 nagios_service_escalation "CONNTRACK" do
   notification_interval 15
 end
-
-# XXX: we do not include shorewall6 by default for now, because the
-# shorewall-perl compiler (which is required for shorewall6) does not work
-# without multiport match support in the kernel, which is missing on almost all
-# machines.
-
-#if node[:ipv6_enabled]
-#  include_recipe "shorewall::ipv6"
-#end
